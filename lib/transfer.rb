846ddf5ff060c024e337da216bffb6928c2d04cc
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-10 06:59:49 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,30 +1,26 @@
 class Transfer
-    attr_accessor :sender, :receiver, :amount, :status
+    attr_accessor :status
+    attr_reader :sender, :receiver, :amount
+    
+    def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = amount
+      @status = "pending"
+    end
 
-  def initialize(sender,receiver,amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = "pending"
-  end
-
-  def valid?
-    @sender.valid? == true && @receiver.valid? == true
-       true   
+    def valid?
+      @sender.valid? == true && @receiver.valid? == true
   end
 
   def execute_transaction
-    if @sender.status == "closed" || @sender.balance < @amount
-      # @sender.display_balance.scan(/\d+/)[0].to_i < @amount
+    if @sender.balance < @amount || valid? == false
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-    elsif @status == "complete"
-      return "Transaction was already executed"
-    else
+    elsif @sender.balance > @amount && status == "pending"
       @sender.deposit(@amount * -1)
       @receiver.deposit(@amount)
       @status = "complete"
-
     end
   end
 
